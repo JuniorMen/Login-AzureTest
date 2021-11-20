2bc54aa9a944f9e928581dd4ff2b4320bc37c323
JuniorMen <roinujstone22@outlook.com>
2021-11-20 14:11:31 -0600

    Cambios

---
+++ test.js
@@ -0,0 +1,11 @@
+var assert =require('assert');
+var login= require('./validation');
+
+describe('probar login',function(){
+        
+    it('prueba contraseña y usuario', function(){
+    assert.equal('luis','luis'  ); 
+
+    });
+
+});
---
+++ validation.js
@@ -0,0 +1,14 @@
+function validate(){
+    var user=document.getElementById("user").value;
+    var password=document.getElementById("password").value;
+
+    if(user == "junior2299@outlook.com" && password == "tacos123"){
+        window.location.href="homepage.html";
+        return;
+        
+    }
+    else{
+        
+    }
+
+}
